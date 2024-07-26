5af07d2870591e56550dfb8e29b4ea9807dfb82c
database-luis <lui.gustavo12@gmail.com>
2024-07-25 22:47:56 -0400

    Adição dos casos de teste no arquivo de teste

--- test/ConversorTest.java
+++ test/ConversorTest.java
@@ -3,5 +3,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConversorTest {
-    
+
+    @Test
+    public void testFahrenheitParaCelsius() {
+        assertEquals(10, Conversor.fahrenheitParaCelsius(50), 0.001);
+    }
+
+    @Test
+    public void testCelsiusParaFahrenheit() {
+        assertEquals(50, Conversor.celsiusParaFahrenheit(10), 0.001);
+    }
 }
